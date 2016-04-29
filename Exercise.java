77b18424d0700e9e5affaca2dc65154801422f37
xieyezi <suyechun@icloud.com>
2016-04-30 00:05:36 +0800

    以后的java程序就在这里了

---
+++ Exercise.java
@@ -0,0 +1,50 @@
+
+class Exercise1_1 {
+    public static void main(String[] args){
+        System.out.println("Welcome to java!");
+        System.out.println("Welcome to Computer Science!");
+        System.out.println("Programming!");
+    }
+}
+
+class Exercise1_2 {
+    public static void main(String[] args){
+        for(int i=0;i<5;i++) {
+           System.out.println("Welcome to java!");
+        }
+    }
+}
+class Exercise1_3 {
+    public static void main(String[] args){
+        System.out.println("    J        A          V     V             A             ");
+        System.out.println("    J       A A          V   V             A A            ");
+        System.out.println("J   J      AA AA          V V             AA AA           ");
+        System.out.println(" J J      A     A          V             A     A          ");
+       
+    }
+} 
+class Exercise1_4 {
+    public static void main(String[] args){
+        System.out.println("a   a^2     a^3");
+        for(int i=1;i<=4;i++){
+        System.out.println(i+"      "+i*i+"     "+i*i*i);
+        }
+    }
+}
+
+class Exercise1_5 {
+    public static void main(String[] args){
+        System.out.println((9.5*4.5-2.5*3)/(45.5-3.5));
+        
+    }
+}
+
+class Exercise1_6 {
+    public static void main(String[] args){
+        int sum=0;
+      for(int i=1;i<10;i++) {
+          sum=sum+i;
+      }
+      System.out.println(sum);
+    }
+}
